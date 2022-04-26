a661d9e2391cd243153d51705fab829f5c553de8
Victoria Perea San Juan <v.vperea@gmail.com>
2022-04-26 17:56:07 +0200

    26-04-22

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,11 @@ $('#toggle').click(function() {
    $(this).toggleClass('active');
    $('#overlay').toggleClass('open');
   });
+
+  $('.js-close-menu').on('click', function() {
+
+    $('#toggle').removeClass('active');
+    
+    $('#overlay').removeClass('open');
+    
+    });
